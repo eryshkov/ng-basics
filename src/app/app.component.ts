576e828ff01e9ae0d0b944f0ad5455644d0896ea
Evgenii Ryshkov <eryshkov@gmail.com>
2020-02-18 14:35:34 +0300

    bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-basics';
+  title = 'Dynamic title';
+  number = 42;
+  arr = [1, 2, 3];
+
+  obj = { a: 1, b: {c: 2}};
+  img = 'https://seeklogo.com/images/R/react-logo-7B3CE81517-seeklogo.com.png';
+
+
+  constructor() {
+    setTimeout(
+      () => {
+        console.log('Timeout is over');
+        this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/240px-Angular_full_color_logo.svg.png';
+      },
+      5000
+    );
+  }
 }
