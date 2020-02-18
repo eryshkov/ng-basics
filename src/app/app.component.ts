f6f346b1a74ce0ea40a86bfd47701743ca3d13ab
Evgenii Ryshkov <eryshkov@gmail.com>
2020-02-18 16:44:52 +0300

    добавлены новые компоненты

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  posts: Post[] = [
+    {title: 'Хочу выучить Anglar компоненты',
+      text: 'Все еще учу компоненты',
+      id: 1
+    },
+    {title: 'Следующий блок',
+      text: 'будет про диррективы и пайпы',
+      id: 2
+    }
+  ];
 }
