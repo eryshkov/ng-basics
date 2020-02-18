3d468b5096fe259ee45517d12d2af9317ef4134a
Evgenii Ryshkov <eryshkov@gmail.com>
2020-02-18 14:54:24 +0300

    bindings из шаблона в код

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,21 @@ export class AppComponent {
   arr = [1, 2, 3];
 
   obj = { a: 1, b: {c: 2}};
-  img = 'https://seeklogo.com/images/R/react-logo-7B3CE81517-seeklogo.com.png';
+  // img = 'https://seeklogo.com/images/R/react-logo-7B3CE81517-seeklogo.com.png';
 
+  inputValue: '';
 
   constructor() {
-    setTimeout(
-      () => {
-        console.log('Timeout is over');
-        this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/240px-Angular_full_color_logo.svg.png';
-      },
-      5000
-    );
+    // setTimeout(
+    //   () => {
+    //     console.log('Timeout is over');
+    //     this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/240px-Angular_full_color_logo.svg.png';
+    //   },
+    //   5000
+    // );
+  }
+
+  onInput(event?) {
+    console.log('Event: ', event);
   }
 }
