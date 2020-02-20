a2080eb1b2c47295df8f19ccd1acc5063f5e14dd
Evgenii Ryshkov <eryshkov@gmail.com>
2020-02-20 15:33:24 +0300

    визуализация состояний

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
     ngOnInit(): void {
         this.form = new FormGroup({
             email: new FormControl('', [Validators.email, Validators.required]),
-            password: new FormControl(''),
+            password: new FormControl('', Validators.minLength(6)),
         });
     }
 
