d8223a068f410300b9c63d9e9a2bb05523243a2a
Evgenii Ryshkov <eryshkov@gmail.com>
2020-02-20 16:08:28 +0300

    отображение сообщений с ошибками

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
     ngOnInit(): void {
         this.form = new FormGroup({
             email: new FormControl('', [Validators.email, Validators.required]),
-            password: new FormControl('', Validators.minLength(6)),
+            password: new FormControl('', [Validators.minLength(6), Validators.required]),
         });
     }
 
