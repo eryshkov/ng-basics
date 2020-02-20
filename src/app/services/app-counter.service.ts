1fb325d853fcc14ce6ac562165315a8a9234fb5a
Evgenii Ryshkov <eryshkov@gmail.com>
2020-02-20 13:36:27 +0300

    связь сервисов

--- src/app/services/app-counter.service.ts
+++ src/app/services/app-counter.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import {LogService} from './log.service';
 
 @Injectable({
     providedIn: 'root'
@@ -6,11 +7,16 @@ import {Injectable} from '@angular/core';
 export class AppCounterService {
     counter = 0;
 
+    constructor(private logService: LogService) {
+    }
+
     increase() {
+        this.logService.log('counter is increased');
         this.counter++;
     }
 
     decrease() {
+        this.logService.log('counter is decreased');
         this.counter--;
     }
 }
