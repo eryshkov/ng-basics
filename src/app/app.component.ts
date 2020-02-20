c42a03a978b87f78629e1cd279618590a775d648
Evgenii Ryshkov <eryshkov@gmail.com>
2020-02-20 12:56:30 +0300

    создание сервиса

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {AppCounterService} from './services/app-counter.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+    constructor(public appCounterService: AppCounterService) {
+    }
 }
