8b59a1171cbb78df3ff2ec282de8cdb073609906
Evgenii Ryshkov <eryshkov@gmail.com>
2020-02-25 13:28:08 +0300

    новый синтаксис импорта

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import {RouterModule} from '@angular/router';
 import {HomePageComponent} from './home-page/home-page.component';
 
 @NgModule({
-  imports: [RouterModule.forRoot([
-    {path: '', component: HomePageComponent, pathMatch: 'full'},
-    {path: 'about', loadChildren: './about-page/about-page.module#AboutPageModule'}
-  ])],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot([
+        {path: '', component: HomePageComponent, pathMatch: 'full'},
+        // {path: 'about', loadChildren: './about-page/about-page.module#AboutPageModule'},
+        {
+            path: 'about', loadChildren: () => import('./about-page/about-page.module').then(m =>
+                m.AboutPageModule
+            )
+        }
+    ])],
+    exports: [RouterModule]
 })
+
 export class AppRoutingModule {
 
 }
