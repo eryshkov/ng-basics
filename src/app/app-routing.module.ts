a8b8733c9faa6f558ec5da0e72eb25410b40600a
Evgenii Ryshkov <eryshkov@gmail.com>
2020-02-25 11:27:43 +0300

    редирект и обработка ошибок

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {AboutComponent} from './about/about.component';
 import {PostsComponent} from './posts/posts.component';
 import {PostComponent} from './post/post.component';
 import {AboutExtraComponent} from './about-extra/about-extra.component';
+import {ErrorPageComponent} from './error-page/error-page.component';
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -13,6 +14,8 @@ const routes: Routes = [
         ]},
     {path: 'posts', component: PostsComponent},
     {path: 'post/:id', component: PostComponent},
+    {path: 'error', component: ErrorPageComponent},
+    {path: '**', redirectTo: '/error'},
 ];
 
 @NgModule({
